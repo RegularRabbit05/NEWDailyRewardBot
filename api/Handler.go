f75b240027f2ec35a435e154933b539883e96b83
RegularRabbit05 <69511985+RegularRabbit05@users.noreply.github.com>
2024-05-22 23:09:21 +0200

    Commit

--- api/Handler.go
+++ api/Handler.go
@@ -1,11 +1,71 @@
 package api
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
+
+// DiscordInteraction is the request we get from Discord when a user
+// triggers a slash Command i.e. /zoom
+type DiscordInteraction struct {
+	Type   float64                  `json:"type"`
+	Data   DiscordInteractionData   `json:"data"`
+	Member DiscordInteractionMember `json:"member"`
+}
+
+// DiscordInteractionData is present for the slash command itself
+// i.e. /zoom
+type DiscordInteractionData struct {
+	Name    string                          `json:"name"`
+	ID      string                          `json:"id"`
+	Type    float64                         `json:"type"`
+	Options []DiscordInteractionDataOptions `json:"options"`
+}
+
+// DiscordInteractionDataOptions contains the option passed in
+// within the slash command i.e. the parameters
+type DiscordInteractionDataOptions struct {
+	Name  string      `json:"name"`
+	Type  float64     `json:"type"`
+	Value interface{} `json:"value"`
+}
+
+type DiscordInteractionMember struct {
+	User DiscordInteractionMemberUser `json:"user"`
+}
+
+// DiscordInteractionMemberUser gives a way to uniquely
+// identify a user by adding # between the Username and
+// the Discriminator
+type DiscordInteractionMemberUser struct {
+	Username      string `json:"username"`
+	Discriminator string `json:"discriminator"`
+}
+
+// DiscordResponse is the response we send back to Discord
+// See also: https://discord.com/developers/docs/interactions/receiving-and-responding
+type DiscordResponse struct {
+	Type float64             `json:"type"`
+	Data DiscordResponseData `json:"data"`
+}
+
+type DiscordResponseData struct {
+	Content string                 `json:"content"`
+	Embeds  []DiscordResponseEmbed `json:"embeds,omitempty"`
+}
+
+type DiscordResponseEmbed struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	Type        string `json:"type"`
+}
 
 const ChannelMessageWithSource = float64(4)
 
@@ -71,4 +131,77 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeResponse(w, commandRes)
+}
+
+type ResponseAPI struct {
+	LastRewardTimestamp int64  `json:"lastRewardTimestamp"`
+	CurrentTimestamp    int64  `json:"currentTimestamp"`
+	LastReward          string `json:"lastReward"`
+	CurrentDate         string `json:"currentDate"`
+	RewardStreak        int    `json:"rewardStreak"`
+	Result              bool   `json:"result"`
+}
+
+func checkDailyReward() ResponseAPI {
+	res, err := http.Get(os.Getenv("api-url"))
+	if err != nil {
+		return ResponseAPI{Result: false}
+	}
+	defer res.Body.Close()
+
+	var apiResponse ResponseAPI
+	if err = json.NewDecoder(res.Body).Decode(&apiResponse); err != nil {
+		return ResponseAPI{Result: false}
+	}
+
+	return apiResponse
+}
+
+func verify(w http.ResponseWriter, r *http.Request, body []byte) {
+	publicKey := os.Getenv("discord-public-key")
+
+	signature := r.Header.Get("X-Signature-Ed25519")
+	timestamp := r.Header.Get("X-Signature-Timestamp")
+
+	signatureHexDecoded, err := hex.DecodeString(signature)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	if len(signatureHexDecoded) != ed25519.SignatureSize {
+		http.Error(w, "invalid signature length", http.StatusUnauthorized)
+		return
+	}
+
+	publicKeyHexDecoded, err := hex.DecodeString(publicKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	pubKey := [32]byte{}
+
+	copy(pubKey[:], publicKeyHexDecoded)
+
+	var msg bytes.Buffer
+	msg.WriteString(timestamp)
+	msg.Write(body)
+
+	verified := ed25519.Verify(publicKeyHexDecoded, msg.Bytes(), signatureHexDecoded)
+
+	if !verified {
+		http.Error(w, "invalid request signature", http.StatusUnauthorized)
+		return
+	}
+
+	p := map[string]float64{
+		"type": float64(1),
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(p); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
